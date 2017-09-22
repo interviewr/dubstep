3112a975e02db74f03750a9885133d4d53515010
Dima Paloskin <dima@paloskin.me>
2017-09-22 18:32:19 +0300

    Skeleton of the flat-tree

--- src/workspace.js
+++ src/workspace.js
@@ -1,4 +1,7 @@
 const { EventEmitter } = require('events')
+const fs = require('mz/fs')
+const onp = require('./onp/onp.js')
+
 const debug = require('debug')('workspace')
 
 const Watcher = require('./watcher')
@@ -7,10 +10,12 @@ class Workspace extends EventEmitter {
   constructor ({ workspacePath }) {
     super()
 
+    this.workspacePath = workspacePath
     this.watcher = new Watcher({
       workspacePath
     })
     this.onUpdate = this.onUpdate.bind(this)
+    this.flatTree = {}
   }
 
   run () {
@@ -19,7 +24,42 @@ class Workspace extends EventEmitter {
     this.watcher.on('update', this.onUpdate)
   }
 
+  async addToFlatTree (type, path, content) {
+    this.flatTree[path] = {
+      type,
+      content
+    }
+  }
+
   async onUpdate (type, path) {
+    switch (type) {
+      case 'addDir': {
+        this.addToFlatTree('dir', path)
+        return
+      }
+
+      case 'add': {
+        try {
+          const content = await fs.readFile(path)
+          this.addToFlatTree('file', path, content)
+        } catch (err) {
+          console.error(err)
+        }
+        return
+      }
+
+      case 'change': {
+        try {
+          const content = await fs.readFile(path)
+          // const prevContent = this.flatTree[path].content
+
+          console.log('diff here')
+          this.addToFlatTree('file', path, content)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    }
   }
 }
 
